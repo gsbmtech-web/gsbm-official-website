687d6e4162f2e4f17bfc2a20ca1e4c218fd2b85b
Syed Luqmaan <syedluqmaan.dev@gmail.com>
2026-05-08 17:39:04 +0530

    Update OTP template to GSBM template

--- api/send-otp.js
+++ api/send-otp.js
@@ -1,4 +1,11 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
@@ -19,7 +26,8 @@ module.exports = async function handler(req, res) {
   const tokenData = `${cleanPhone}:${otp}:${expiry}`;
   const token = Buffer.from(tokenData).toString('base64');
 
-  const message = `${otp} is your One Time Password (OTP) for AVIT. This OTP is valid till 10 mins - Team Admissions AVITEG`;
+  // Message matching NEW DLT template exactly
+  const message = `${otp} is your OTP. This OTP is valid for the next 10 minutes. Team Admissions GSBMCH`;
   const encodedMessage = encodeURIComponent(message);
   const apiUrl = `http://pay4sms.in/sendsms/?token=${process.env.PAY4SMS_TOKEN}&credit=4&sender=${process.env.PAY4SMS_SENDER}&message=${encodedMessage}&number=${cleanPhone}&templateid=${process.env.PAY4SMS_TEMPLATE_ID}`;
 
@@ -38,4 +46,4 @@ module.exports = async function handler(req, res) {
     console.error('Error:', error);
     return res.status(500).json({ success: false, message: 'Server error. Please try again.' });
   }
-};
+}
